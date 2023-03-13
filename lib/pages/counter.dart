de00aa2871b208ad823a890800e72114107ba8af
Hejo <59996938+codezay@users.noreply.github.com>
2023-03-13 18:16:34 +0530

    counter app using provider

---
+++ lib/pages/counter.dart
@@ -0,0 +1,32 @@
+import 'package:counter_app_using_provider/provider/counter_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    final counterProvider = Provider.of<CounterProvider>(context);
+    final count = counterProvider.count;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Page'),
+      ),
+      body: Center(
+        child: Text(
+          'Button press count: $count',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Counter button pressed');
+          counterProvider.incrementCount();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
